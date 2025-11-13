074bfa24a5b305e82e1d13d291c72e87d1b7c6f1
KawanCosta <kawancosta223@gmail.com>
2025-11-13 12:03:07 -0300

    menus

--- setup.php
+++ setup.php
@@ -3,7 +3,7 @@
 
 use GlpiPlugin\Equipmentexit\Request as PluginEquipmentexitRequest;
 
-define('PLUGIN_EQUIPMENTEXIT_VERSION', '3.8.0'); // v3.8
+define('PLUGIN_EQUIPMENTEXIT_VERSION', '3.8.1'); // v3.8
 define("PLUGIN_EQUIPMENTEXIT_MIN_GLPI_VERSION", "10.0.0");
 
 function plugin_init_equipmentexit() {
@@ -11,61 +11,58 @@ function plugin_init_equipmentexit() {
     $plugin_name = 'equipmentexit';
 
     Plugin::registerClass(PluginEquipmentexitRequest::class);
+    
+    // *** ATUALIZADO: Registra as novas classes de menu ***
+    Plugin::registerClass('PluginEquipmentexitMenu_Approvals');
+    Plugin::registerClass('PluginEquipmentexitMenu_Requests');
 
     $PLUGIN_HOOKS['csrf_compliant'][$plugin_name] = true;
-    
-    // *** INÍCIO DA CORREÇÃO (v3.8) ***
     
     // 1. Menu do Solicitante (Helpdesk)
     $PLUGIN_HOOKS['menu_toadd'][$plugin_name]['knowbase'] = 'PluginEquipmentexitMenu';
     
-    // 2. Menu do Aprovador (Plugins)
-    //    Só aparece se o usuário for Admin OU estiver nas tabelas de papel
-    if (PluginEquipmentexitMenu::canViewApprovalQueue($DB)) {
-        $PLUGIN_HOOKS['menu_toadd'][$plugin_name]['plugins'] = 'PluginEquipmentexitApprovalMenu';
-    }
-    
+    // 2. Menus do Aprovador (Plugins) - Agora separados e sem verificação de IF
+    $PLUGIN_HOOKS['menu_toadd'][$plugin_name]['plugins']['approvals'] = 'PluginEquipmentexitMenu_Approvals';
+    $PLUGIN_HOOKS['menu_toadd'][$plugin_name]['plugins']['requests']  = 'PluginEquipmentexitMenu_Requests';
+
     // 3. Menu de Configuração (Configuração > Geral)
-    //    Só aparece para Super-Admins
     if (Session::haveRight('config', UPDATE)) {
          $PLUGIN_HOOKS['menu_toadd'][$plugin_name]['setup'] = 'PluginEquipmentexitConfigMenu';
     }
-    // *** FIM DA CORREÇÃO ***
     
     $PLUGIN_HOOKS['add_css'][$plugin_name] = 'css/equipmentexit.css';
 }
 
 /**
- * Menu Padrão (Minhas Solicitações, Nova Solicitação)
+ * Menu Padrão (Helpdesk: Minhas Solicitações, Nova Solicitação)
  * Visto por Solicitantes (self-service) e Admins.
  */
 class PluginEquipmentexitMenu {
     
+ 
     static function getMenuContent() {
         global $plugin_name;
         $plugin_name = 'equipmentexit';
-        $links = [];
+        $links = array(); 
         
-        $links['search'] = [ 
+        $links['search'] = array( 
             'title' => __('Minhas Solicitações', $plugin_name),
             'page'  => '/plugins/equipmentexit/front/request.php',
             'icon'  => 'fas fa-list'
-        ];
+        );
         
-        $links['add'] = [ 
+        $links['add'] = array( 
             'title' => __('Nova Solicitação', $plugin_name),
             'page'  => '/plugins/equipmentexit/front/request.form.php',
             'icon'  => 'fas fa-plus'
-        ];
+        );
         
-        // O link "Configurar" foi REMOVIDO daqui.
-
-        return [
+        return array( 
             'title'       => __('Saída de Equipamentos', $plugin_name), 
             'page'        => '/plugins/equipmentexit/front/request.php', 
             'icon'        => 'fas fa-sign-out-alt', 
             'links'       => $links,
-        ];
+        );
     }
 
      static function getMenuName($nb = 0) {
@@ -74,7 +71,8 @@ class PluginEquipmentexitMenu {
      }
      
      /**
-      * (v3.7) Função de verificação de permissão (SQL Bruto - Correta)
+      * (v3.7) Função de verificação de permissão (SQL)
+      * Mantida aqui, mas não está mais bloqueando o menu de plugins.
       */
      static function canViewApprovalQueue($DB) {
         if (Session::haveRight('config', UPDATE)) {
@@ -82,13 +80,13 @@ class PluginEquipmentexitMenu {
         }
         $user_id = Session::getLoginUserID();
         if (empty($user_id)) {
-            return false;
+             return false;
         }
-        $tables_to_check = [
+        $tables_to_check = array( 
             'glpi_plugin_equipmentexit_gerentes',
             'glpi_plugin_equipmentexit_governanca',
             'glpi_plugin_equipmentexit_seguranca'
-        ];
+        );
         foreach ($tables_to_check as $table_name) {
             $sql_check = "SELECT COUNT(*) as cpt FROM `$table_name` WHERE `users_id` = " . (int)$user_id;
             $check_result = $DB->query($sql_check);
@@ -100,32 +98,63 @@ class PluginEquipmentexitMenu {
      }
 }
 
+// *** CLASSE ANTIGA 'PluginEquipmentexitApprovalMenu' REMOVIDA ***
+
 /**
- * (v3.7) Menu da Fila de Ações
- * Visto por Aprovadores e Admins (no menu "Plugins").
+ * NOVO: Menu "Saídas - Aprovações" (Aba Plug-ins)
  */
-class PluginEquipmentexitApprovalMenu {
+class PluginEquipmentexitMenu_Approvals {
     static function getMenuContent() {
         global $plugin_name;
         $plugin_name = 'equipmentexit';
-        $links = [];
-        $links['approval_queue'] = [ 
-            'title' => __('Fila de Ações', $plugin_name),
-            'page'  => '/plugins/equipmentexit/front/approval.php',
-            'icon'  => 'fas fa-tasks'
-        ];
-        return [
-            'title'       => __('Fila de Ações', $plugin_name), 
+
+        return array(
+            'title'       => __('Saídas - Aprovações', $plugin_name), 
             'page'        => '/plugins/equipmentexit/front/approval.php', 
-            'icon'        => 'fas fa-tasks', 
-            'links'       => $links,
-        ];
+            'icon'        => 'fas fa-tasks',
+            'links'       => array() // Sem sub-menu
+        );
     }
-     static function getMenuName($nb = 0) {
-         $content = self::getMenuContent();
-         return $content['title'] ?? __('Fila de Ações', 'equipmentexit');
-     }
+
+    static function getMenuName($nb = 0) {
+         return __('Saídas - Aprovações', 'equipmentexit');
+    }
 }
+
+/**
+ * NOVO: Menu "Saídas - Requisições" (Aba Plug-ins)
+ */
+class PluginEquipmentexitMenu_Requests {
+    static function getMenuContent() {
+        global $plugin_name;
+        $plugin_name = 'equipmentexit';
+
+        // Links internos (Minhas Requisições / Nova)
+        $links = array();
+        $links['my_requests'] = array( 
+            'title' => __('Minhas Solicitações', $plugin_name),
+            'page'  => '/plugins/equipmentexit/front/request.php',
+            'icon'  => 'fas fa-list'
+        );
+        $links['new_request'] = array( 
+            'title' => __('Nova Solicitação', $plugin_name),
+            'page'  => '/plugins/equipmentexit/front/request.form.php',
+            'icon'  => 'fas fa-plus'
+        );
+
+        return array(
+            'title'       => __('Saídas - Requisições', $plugin_name), 
+            'page'        => '/plugins/equipmentexit/front/request.php', 
+            'icon'        => 'fas fa-list',
+            'links'       => $links
+        );
+    }
+
+    static function getMenuName($nb = 0) {
+         return __('Saídas - Requisições', 'equipmentexit');
+    }
+}
+
 
 /**
  * (v3.8) NOVO MENU DE CONFIGURAÇÃO
@@ -135,23 +164,23 @@ class PluginEquipmentexitConfigMenu {
     static function getMenuContent() {
         global $plugin_name;
         $plugin_name = 'equipmentexit';
-        $links = [];
+        $links = array(); 
 
         // O link agora aponta para a página de configuração
-        $links['config'] = [
-            'title' => __('Saída de Equipamentos', $plugin_name), // Nome do plugin
+        $links['config'] = array( 
+             'title' => __('Saída de Equipamentos', $plugin_name), // Nome do plugin
             'page'  => '/plugins/equipmentexit/front/config.php',
             'icon'  => 'fas fa-sign-out-alt'
-        ];
+        );
 
-        return [
+        return array( 
             // Este título não aparece, mas é necessário
             'title'       => __('Saída de Equipamentos', $plugin_name), 
             'page'        => '/plugins/equipmentexit/front/config.php',
             'links'       => $links,
             'icon'        => 'fas fa-sign-out-alt',
             'SUB_MENU'    => 'config' // Adiciona ao menu "Geral"
-        ];
+        );
     }
     
      static function getMenuName($nb = 0) {
@@ -162,13 +191,13 @@ class PluginEquipmentexitConfigMenu {
 
 // --- Funções de Versão e Verificação (Inalteradas) ---
 function plugin_version_equipmentexit() {
-    return [
+    return array( 
         'name'           => __('Saída de Equipamentos', 'equipmentexit'),
         'version'        => PLUGIN_EQUIPMENTEXIT_VERSION,
         'author'         => 'Kawan Costa', 
         'license'        => 'GPLv3', 
         'minGlpiVersion' => PLUGIN_EQUIPMENTEXIT_MIN_GLPI_VERSION
-    ];
+    );
 }
 function plugin_equipmentexit_check_config($verbose = false) {
     return true;
